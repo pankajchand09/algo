2168e1adad2c0c53aac79ee214caf85746d012bd
Pankaj chand <pankajchand09@gmail.com>
2017-01-12 20:33:59 +0530

    Tries including values

--- src/tries/Tries.java
+++ src/tries/Tries.java
@@ -4,21 +4,24 @@ public class Tries {
 public static TriesNode root;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-String[] allString={"myname","name"};
+String[] allString={"myname","name","is","pankaj","ankaj"};
 
 for(String str:allString){
 	if(root==null)
 		root=new TriesNode();
-	root=addElement(str, 0, root);
+	root=addElement(str, 0, root,str.length());
 }
+
 for(String str:allString){
 	 str=str;
 	int index=str.charAt(0)-97;
-System.out.println(str+"::"+getElement(str, 0, root.childNode[index]));
-}	
+	for(int l=1;l<=str.length();++l)
+System.out.println(str.substring(0,l)+"::"+isExist(str.substring(0, l), 0, root.childNode[index]));
+
+}
 }
 	
-public static TriesNode addElement(String str,int pos,TriesNode triNode){
+public static TriesNode addElement(String str,int pos,TriesNode triNode,int value){
 
 	if(pos<str.length()){
 	int index=str.charAt(pos)-97;
@@ -26,21 +29,30 @@ public static TriesNode addElement(String str,int pos,TriesNode triNode){
 	if(triNode.childNode[index]==null){
 		triNode.childNode[index]=new TriesNode(str.charAt(pos));
 	}
-	 triNode.childNode[index]=addElement(str, pos+1, triNode.childNode[index]);
-	
+	 triNode.childNode[index]=addElement(str, pos+1, triNode.childNode[index],value);
+	if(pos==str.length()-1)
+		triNode.childNode[index].setValue(value);
 	}
 		return triNode;
 	
 }
-public static boolean getElement(String str,int pos,TriesNode triNode){
-	System.out.println(str+":::"+pos);
+public static boolean isExist(String str,int pos,TriesNode triNode){
+	TriesNode node=getElement(str, pos, triNode);
+	boolean exist=false;
+	if(node!=null&&node.getValue()>0)
+		exist=true;
+	return exist;
+		
+}
+public static TriesNode getElement(String str,int pos,TriesNode triNode){
+	
 	if(triNode==null)
-		return false;
+		return null;
 	{
 		int index=str.charAt(pos)-97;
 	   {
 		   if(pos==str.length()-1)
-			   return true;
+			   return triNode;
 		   else
 			   return getElement(str, pos+1, triNode.childNode[str.charAt(pos+1)-97]);
 	   }
--- src/tries/TriesNode.java
+++ src/tries/TriesNode.java
@@ -1,8 +1,11 @@
 package tries;
 
+import java.util.Arrays;
+
 public class TriesNode {
 int value;
 char ch;
+
 TriesNode childNode[];
 public TriesNode(char ch) {
 	this.ch=ch;
@@ -18,6 +21,10 @@ public TriesNode() {
 	
 }
 
+@Override
+public String toString() {
+	return "TriesNode [value=" + value + ", ch=" + ch +"]";
+}
 public int getValue() {
 	return value;
 }
